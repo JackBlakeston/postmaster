723e174145aefc59a7e3c5ad6aa6e1048c0594d5
Jack Blakeston <jackblakeston93@gmail.com>
2022-07-16 00:44:23 +0200

    006-feat: implement responsiveness and logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,14 @@
 import styles from './Navbar.module.scss';
 import NewPostButton from '../NewPostButton/NewPostButton';
+import Logo from '../../assets/images/postmaster.png';
 
 const Navbar = () => {
 
   return (
     <div className={styles.mainContainer}>
+      {/* <div className={styles.logoContainer}> */}
+      <img className={styles.logo} src={Logo}/>
+      {/* </div> */}
       <NewPostButton/>
     </div>
   );
