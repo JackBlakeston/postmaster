f35a3e79ed82011e0cb0b77b38a9632c3b8ebf22
Jack Blakeston <jackblakeston93@gmail.com>
2022-07-20 02:03:47 +0200

    009-feat: implement debounce on search box

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,16 +1,22 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 
-import {  TEXT } from '../../constants';
+import { TEXT } from '../../constants';
 import { useAppDispatch } from '../../redux/hooks';
 import { searchModified } from '../../slices/filters/filtersSlice';
 import styles from './SearchBox.module.scss';
+import { useDebounce } from './utils';
 
 const SearchBox = () => {
 
   const dispatch = useAppDispatch();
+  const [search, setSearch] = useState('');
+
+  useDebounce(() => {
+    dispatch(searchModified(search));
+  }, [search], 300);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    dispatch(searchModified(event.target.value));
+    setSearch(event.target.value);
   };
 
   return (
